a5c38cd74528ec7c4514c0776ce0bb6b14e30e0a
Aybars Acar <aybars.dev@gmail.com>
2021-09-05 21:45:33 +1000

    Dynamic Reticle added

---
+++ FPS/Reticle.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+namespace Dead_Earth.Scripts.FPS
+{
+  /// <summary>
+  /// dynamically renders / changes the size of the reticle
+  /// based on the player speed 
+  /// </summary>
+  public class Reticle : MonoBehaviour
+  {
+    [SerializeField] private FPSController player;
+
+    private RectTransform _rect;
+    private float _currentRectSize;
+
+    private const float RunningRectSize = 150;
+    private const float WalkingRectSize = 100;
+
+    private void Awake()
+    {
+      _rect = GetComponent<RectTransform>();
+
+      _currentRectSize = WalkingRectSize;
+    }
+
+    private void Update()
+    {
+      if (player.MovementStatus == PlayerMoveStatus.Running || player.MovementStatus == PlayerMoveStatus.NotGrounded)
+      {
+        _currentRectSize = Mathf.Lerp(_currentRectSize, RunningRectSize, Time.deltaTime * 2f);
+      }
+      else
+      {
+        _currentRectSize = Mathf.Lerp(_currentRectSize, WalkingRectSize, Time.deltaTime * 2f);
+      }
+
+      _rect.sizeDelta = new Vector2(_currentRectSize, _currentRectSize);
+    }
+  }
+}
