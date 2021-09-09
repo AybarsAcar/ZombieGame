d4387e6cdda9685bd2a23629da4e3d4f60418b5a
Aybars Acar <aybars.dev@gmail.com>
2021-09-09 20:55:40 +1000

    Audio Pooling System implemented

--- Audio/AudioManager.cs
+++ Audio/AudioManager.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 namespace Dead_Earth.Scripts.Audio
 {
   /// <summary>
   /// stores all the information about an Audio Mixer Group
-  /// contains the name of the group which is exposed 
+  /// contains the name of the group which is exposed
   /// </summary>
   public class TrackInfo
   {
@@ -17,7 +18,26 @@ namespace Dead_Earth.Scripts.Audio
   }
 
   /// <summary>
+  /// Describes an audio entity in the pooling system
+  /// </summary>
+  public class AudioPoolItem
+  {
+    public GameObject gameObject;
+    public Transform transform;
+    public AudioSource audioSource;
+    public float unimportance = float.MaxValue; // used by the priority system
+    public bool isPlaying;
+    public IEnumerator coroutine;
+
+    // generated every single time a sound request is made
+    // id will be unique each time it plays a sound
+    // so this will change over time as we reuse the object to play other sounds
+    public ulong id = 0;
+  }
+
+  /// <summary>
   /// Manages the Audios in the Game - it is a singleton
+  /// provides a pooling system for the Audio's in the scene
   /// </summary>
   public class AudioManager : MonoBehaviour
   {
@@ -38,7 +58,17 @@ namespace Dead_Earth.Scripts.Audio
 
     [SerializeField] private AudioMixer mixer;
 
+    [Tooltip("Pool size of the Audio Manager")] [SerializeField]
+    private int maxSounds = 10;
+
     private Dictionary<string, TrackInfo> _tracks = new Dictionary<string, TrackInfo>();
+    private List<AudioPoolItem> _pool = new List<AudioPoolItem>();
+
+    // stores the currently playing / active sounds from our pool list
+    private Dictionary<ulong, AudioPoolItem> _activePool = new Dictionary<ulong, AudioPoolItem>();
+
+    private ulong _idGiver = 0;
+    private Transform _listenerPosition; // current audio listener position
 
     private void Awake()
     {
@@ -61,6 +91,49 @@ namespace Dead_Earth.Scripts.Audio
 
         _tracks.Add(mixerGroup.name, trackInfo);
       }
+
+      // Generate the pool
+      for (int i = 0; i < maxSounds; i++)
+      {
+        var gameObj = new GameObject("Pool Item");
+        var audioSource = gameObj.AddComponent<AudioSource>();
+
+        // add it as a child to the AudioManger
+        gameObj.transform.parent = transform;
+
+        var poolItem = new AudioPoolItem
+        {
+          gameObject = gameObj,
+          audioSource = audioSource,
+          transform = gameObj.transform,
+          isPlaying = false,
+        };
+
+        gameObj.SetActive(false);
+
+        _pool.Add(poolItem);
+      }
+    }
+
+    private void OnEnable()
+    {
+      SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+      SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    /// <summary>
+    /// a new scene has just been loaded so we need to find the listener
+    /// </summary>
+    /// <param name="scene"></param>
+    /// <param name="mode"></param>
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+      // cache the audio listener position in the scene
+      _listenerPosition = FindObjectOfType<AudioListener>().transform;
     }
 
     /// <summary>
@@ -163,6 +236,173 @@ namespace Dead_Earth.Scripts.Audio
       // this line is to be safe against floating point precision
       // this is ideally achieved in the while loop above
       mixer.SetFloat(track, volume);
+    }
+
+    /// <summary>
+    /// Plays a One Shot sound
+    /// </summary>
+    /// <param name="track"></param>
+    /// <param name="clip"></param>
+    /// <param name="position"></param>
+    /// <param name="volume"></param>
+    /// <param name="spatialBlend"></param>
+    /// <param name="priority">used to generate unimportance value</param>
+    /// <returns></returns>
+    public ulong PlayOneShotSound(string track, AudioClip clip, Vector3 position, float volume, float spatialBlend,
+      int priority = 128)
+    {
+      if (!_tracks.ContainsKey(track) || clip == null || volume.Equals(0f))
+      {
+        return 0;
+      }
+
+      // less it is better it is
+      var unimportance = (_listenerPosition.position - position).sqrMagnitude / Mathf.Max(1, priority);
+
+      var leastImportantIndex = -1;
+      var leastImportanceValue = float.MaxValue;
+
+      for (int i = 0; i < _pool.Count; i++)
+      {
+        var poolItem = _pool[i];
+
+        // not playing audio pool item is available
+        if (!poolItem.isPlaying)
+        {
+          return ConfigurePoolObject(i, track, clip, Vector3.positiveInfinity, volume, spatialBlend, unimportance);
+        }
+
+        // the current pool audio item is playing but less important than our new audio
+        if (poolItem.unimportance > leastImportanceValue)
+        {
+          leastImportanceValue = poolItem.unimportance;
+          leastImportantIndex = i;
+        }
+      }
+
+      // all the pool items are playing a sound but we have a less important audio playing
+      // so we stop the least important audio to play the new audio if the new audio is not the least important one
+      if (leastImportanceValue > unimportance)
+      {
+        return ConfigurePoolObject(leastImportantIndex, track, clip, Vector3.positiveInfinity, volume, spatialBlend,
+          unimportance);
+      }
+
+      // our audio is less important the the least important in our currently playing list
+      // the new audio is not played
+      return 0;
+    }
+
+    /// <summary>
+    /// Queue up a one shot sound to be played after a number of seconds
+    /// </summary>
+    /// <param name="track"></param>
+    /// <param name="clip"></param>
+    /// <param name="position"></param>
+    /// <param name="volume"></param>
+    /// <param name="spatialBlend"></param>
+    /// <param name="duration"></param>
+    /// <param name="priority"></param>
+    /// <returns></returns>
+    public IEnumerator PlayOneShotSound(string track, AudioClip clip, Vector3 position, float volume,
+      float spatialBlend, float duration, int priority = 128)
+    {
+      yield return new WaitForSeconds(duration);
+
+      PlayOneShotSound(track, clip, position, volume, spatialBlend, priority);
+    }
+
+    /// <summary>
+    /// Stops the currently playing sound passed in the id
+    /// </summary>
+    /// <param name="id"></param>
+    public void StopOneShotSound(ulong id)
+    {
+      if (_activePool.TryGetValue(id, out var activeSound))
+      {
+        StopCoroutine(activeSound.coroutine);
+
+        activeSound.audioSource.Stop();
+        activeSound.audioSource.clip = null;
+        activeSound.gameObject.SetActive(false);
+
+        // remove from the active list
+        _activePool.Remove(id);
+
+        activeSound.isPlaying = false;
+      }
+    }
+
+    /// <summary>
+    /// this will play a one shot sound from our Pool List
+    /// </summary>
+    /// <param name="poolIndex"></param>
+    /// <param name="track"></param>
+    /// <param name="clip"></param>
+    /// <param name="position"></param>
+    /// <param name="volume"></param>
+    /// <param name="spatialBlend"></param>
+    /// <param name="unimportance"></param>
+    /// <returns></returns>
+    protected ulong ConfigurePoolObject(int poolIndex, string track, AudioClip clip, Vector3 position, float volume,
+      float spatialBlend, float unimportance)
+    {
+      if (poolIndex < 0 || poolIndex >= _pool.Count) return 0;
+
+      var poolItem = _pool[poolIndex];
+
+      _idGiver++;
+
+      var source = poolItem.audioSource;
+      source.clip = clip;
+      source.volume = volume;
+      source.spatialBlend = spatialBlend;
+
+      // assign to requested audio group / track
+      source.outputAudioMixerGroup = _tracks[track].group;
+
+      source.transform.position = position;
+
+      poolItem.isPlaying = true;
+      poolItem.unimportance = unimportance;
+      poolItem.id = _idGiver;
+      poolItem.gameObject.SetActive(true);
+
+      source.Play();
+
+      poolItem.coroutine = StopSoundDelayed(_idGiver, source.clip.length);
+      StartCoroutine(poolItem.coroutine);
+
+      // add the audio pool item to the active audio's pool 
+      _activePool.Add(_idGiver, poolItem);
+
+      // return the id to the called
+      return _idGiver;
+    }
+
+    /// <summary>
+    /// plays the audio as its duration
+    /// then stops the sound and cleans up the audio pool item
+    /// before removing from the active audio pool list
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="duration"></param>
+    /// <returns></returns>
+    private IEnumerator StopSoundDelayed(ulong id, float duration)
+    {
+      yield return new WaitForSeconds(duration);
+
+      if (_activePool.TryGetValue(id, out var poolItem))
+      {
+        // stop the audio and cleanup before removing from the active dictionary 
+        poolItem.audioSource.Stop();
+        poolItem.audioSource.clip = null;
+        poolItem.gameObject.SetActive(false);
+
+        _activePool.Remove(id);
+
+        poolItem.isPlaying = false;
+      }
     }
   }
 }
--- Audio/AudioManagerTest.cs
+++ Audio/AudioManagerTest.cs
@@ -4,12 +4,24 @@ namespace Dead_Earth.Scripts.Audio
 {
   public class AudioManagerTest : MonoBehaviour
   {
+    public AudioClip clip;
+
+
     private void Start()
     {
       if (AudioManager.Instance)
       {
         AudioManager.Instance.SetTrackVolume("Zombies", 10, 5);
+
+        InvokeRepeating(nameof(PlayTest), 1, 1);
       }
+    }
+
+
+    void PlayTest()
+    {
+      // 0 spatial blend means 2D 
+      AudioManager.Instance.PlayOneShotSound("Player", clip, transform.position, 0.5f, 0f, 128);
     }
   }
 }
